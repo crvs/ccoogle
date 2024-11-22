7eb581191af23e049a56364854f4cbcb0a1c66f0
crvs <crvs@crvs.xyz>
2024-11-22 23:37:12 +0100

    Emit quickfix format and support multiple files

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-C approximation of coogle using stb_c_lexer
+C approximation of coogle
 =====
 
-example of fuzzy matching on signature:
+To build should be enough to do:
 
+```bash
+$ make
 ```
-$ <list files> | xargs -n1 ccoogle | fzf
+Example of fuzzy matching on signature:
+
+```bash
+$ xargs ccoogle <list files> | fzf
 ```
 
+or from vim do
+
+```vim
+:!ccoogle /usr/X11R6/include/*.h /usr/X11R6/include/**/*.h > X11R6_signatures.lst
+:cfile X11R6_signatures.lst
+```
+
+This populates the quickfix window with a list of signatures from the `X11R6` library, so that pressing enter on a line transports you to the corresponding signature.
--- ccoogle.c
+++ ccoogle.c
@@ -1,68 +1,95 @@
-
 #define STB_C_LEXER_IMPLEMENTATION
 #include "stb_c_lexer.h"
 
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#define SIZE 600000
 
-int mysqrt(float* floater, double * const * doubler);
-int main(int argc, char** argv) {
-    char* file_name = argv[1];
-    FILE* handle = fopen(file_name, "r");
+#define INPUT_MAX_SIZE 600000
+#define SIG_COMPONENTS 100
+#define SIG_COMP_SIZE 100
 
-    char input[SIZE] = {0};
-    char signature[100][100] = {0};
+void processFile(char* file_path)
+{
+    FILE* handle = fopen(file_path, "r");
 
-    size_t end = fread(input, 1, SIZE, handle);
+    char input[INPUT_MAX_SIZE] = {0};
+    char signature[SIG_COMPONENTS][SIG_COMP_SIZE] = {0};
 
-    char storage[SIZE] = {0};
+    size_t end = fread(input, 1, INPUT_MAX_SIZE, handle);
+
+    char storage[INPUT_MAX_SIZE] = {0};
     stb_lexer lexer;
-    stb_c_lexer_init(&lexer, input, &input[end], storage, SIZE);
+    stb_c_lexer_init(&lexer, input, &input[end], storage, INPUT_MAX_SIZE);
 
     int i = 0;
-    int have_open_paren = false;
-    int have_close_paren = false;
+    int have_open_paren = 0;
+    int have_close_paren = 0;
+    int paren_depth = 0;
+    int min_paren_depth = 0;
+    char* signature_start;
+
     while(stb_c_lexer_get_token(&lexer))
     {
 
+        if (i == 0)
+        {
+            signature_start = lexer.where_firstchar;
+        }
+
         switch (lexer.token)
         {
+            // TODO: to get this to handle C++ we need to be able to handle <, > and :: for template matching
             case CLEX_id:
-                strlcpy(signature[i++], lexer.string, 100);
+                strlcpy(signature[i++], lexer.string, SIG_COMP_SIZE);
                 break;
             case ')':
                 signature[i++][0] = lexer.token;
-                have_close_paren = true;
+                paren_depth--;
+                if (min_paren_depth < paren_depth) min_paren_depth = paren_depth;
+                have_close_paren++;
                 break;
             case '(':
                 signature[i++][0] = lexer.token;
-                have_open_paren = true;
+                paren_depth++;
+                have_open_paren++;
                 break;
             case '*':
             case ',':
                 signature[i++][0] = lexer.token;
                 break;
             default:
-               if (have_open_paren && have_close_paren)
+               /// emit signature if valid
+               if (
+                       have_open_paren /* at least one parenthesis */ &&
+                       have_open_paren == have_close_paren && min_paren_depth == 0 /* parenthesis properly nested */ &&
+                       strcmp(signature[0], "typedef") /* not a typedef*/ )
                {
+                   stb_lex_location loc = {0};
+                   stb_c_lexer_get_location(&lexer, signature_start, &loc);
+                   printf("%s|%d| ", file_path, loc.line_number);
                    for (int j = 0; j < i; ++j)
                    {
+                       // ignore extern keyword
+                       if (j == 0 && strcmp(signature[j], "extern") == 0) continue;
                        printf("%s ", signature[j]);
                    }
                    printf("\n");
                }
+               /// reset data for next candidate signature
+               min_paren_depth = 0;
+               have_open_paren = 0;
+               have_close_paren = 0;
                for (int j = 0; j < i; ++j)
                {
-                   have_open_paren = false;
-                   have_close_paren = false;
-                   memset(signature[j], 0, 100);
+                   memset(signature[j], 0, SIG_COMP_SIZE);
                }
                i = 0;
         }
     }
+}
 
-
+int main(int argc, char** argv) {
+    for (int i = 1; i < argc; ++i) processFile(argv[i]);
     return 0;
 }
